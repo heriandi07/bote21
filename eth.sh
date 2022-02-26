44df2375e4c70821e0aa4286aedffa46d6ea9ff0
bolog123 <97966372+bolog123@users.noreply.github.com>
2022-02-26 19:15:41 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -13,7 +13,7 @@ WALLET=0x23dd47f22240551895b02196973f348714883e33.lolbote21
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool ssl://eth-us-east.flexpool.io:5555 $pool2 ssl://eth-hke.flexpool.io:5555 --user 0x3b4bF032b514ABC3734149f20Cc1EB3b00EEBeCf.RIG32
 while [ $? -eq 42 ]; do
     sleep 10s
     ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
